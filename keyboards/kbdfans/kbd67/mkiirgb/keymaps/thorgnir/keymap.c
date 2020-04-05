b7c0013fc09afa54ed401e73f781a456f7b54ae6
Stepan Milyaev <milyaevs@quantumart.ru>
2020-04-05 19:22:00 +0300

    move arrows to ijkl

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/thorgnir/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/thorgnir/keymap.c
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		  KC_LSFT,            KC_Z,     KC_X,     KC_C,       KC_V,       KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,           KC_UP,     KC_END,\
 		  KC_LCTL,            KC_LGUI,  KC_LALT,                KC_SPC,                MO(1),             KC_RCTL,  KC_LEFT,            KC_DOWN, KC_RIGHT),
 		[_LAYER1] = LAYOUT_65_ansi_blocker( /* FN */
-			KC_GRAVE,            KC_F1,    KC_F2,    KC_F3,      KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_HOME,\
-      KC_TRNS,            KC_TRNS,  KC_HOME,  KC_UP,      KC_END,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK,  KC_PAUS, RESET,    KC_PGUP,\
-			CTL_T(KC_CAPS),     KC_TRNS,  KC_LEFT,  KC_DOWN,    KC_RIGHT,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          EEP_RST,  KC_PGDN,\
-      KC_LSFT,            KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_VOLU,  KC_MUTE,\
+			KC_GRAVE,           KC_F1,    KC_F2,    KC_F3,      KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_HOME,\
+      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_HOME, KC_UP,   KC_END,   KC_PSCR,  KC_SLCK,  KC_PAUS, RESET,    KC_PGUP,\
+			CTL_T(KC_CAPS),     KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS,  KC_TRNS,          EEP_RST,  KC_PGDN,\
+      KC_LSFT,            KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,          KC_VOLU,  KC_MUTE,\
       KC_TRNS,            KC_TRNS,  KC_TRNS,                  KC_TRNS,                   KC_TRNS,          KC_TRNS,  KC_MPRV,          KC_VOLD,  KC_MNXT),
 };
 void matrix_init_user(void)
